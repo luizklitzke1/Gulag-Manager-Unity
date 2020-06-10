72a19b887bf0776ac436a3d8ff7397e93596092e
ArthurBPinotti <arthurbpinotti@gmail.com>
2020-06-09 21:01:07 -0300

    Continua com problema Get Component

--- Gulag Manager/Assets/Scripts/ControladorGame.cs
+++ Gulag Manager/Assets/Scripts/ControladorGame.cs
@@ -8,43 +8,42 @@ using TMPro;
 
 public class ControladorGame : MonoBehaviour
 {
-
     public static Gulag gulag_game = Controlador.gulag_atual;
-    
-    public  Upgrade[] medic_upgs = new Upgrade[] {};
-    public  Upgrade[] recur_upgs = new Upgrade[] {};
-    public  Upgrade[] segur_upgs = new Upgrade[] {};
+
+    public Upgrade[] medic_upgs = new Upgrade[] { };
+    public Upgrade[] recur_upgs = new Upgrade[] { };
+    public Upgrade[] segur_upgs = new Upgrade[] { };
 
     //Upgrades estrutura
-    public static Upgrade upg_cimento = new Upgrade("Cimento do Scarpelly","Krlo, Scarpelly as 3am fazendo cimento",
+    public static Upgrade upg_cimento = new Upgrade("Cimento do Scarpelly1", "Krlo, Scarpelly as 3am fazendo cimento",
                     420.69f,
-                    (new object[,] {{"carlos",13},}),
-                    (new object[,] {{"+Cimento do Scarpelly",1},})
+                    (new object[,] { { "carlos", 13 }, }),
+                    (new object[,] { { "+Cimento do Scarpelly1", 1 }, })
                     );
-    public static Upgrade upg_cimento2 = new Upgrade("Cimento do Scarpelly","Krlo, Scarpelly as 3am fazendo cimento",
+    public static Upgrade upg_cimento2 = new Upgrade("Cimento do Scarpelly2", "Krlo, Scarpelly as 3am fazendo cimento",
                     420.69f,
-                    (new object[,] {{"carlos",13},}),
-                    (new object[,] {{"+Cimento do Scarpelly",1},})
+                    (new object[,] { { "carlos", 13 }, }),
+                    (new object[,] { { "+Cimento do Scarpelly2", 1 }, })
                     );
-    public static Upgrade upg_cimento3 = new Upgrade("Cimento do Scarpelly","Krlo, Scarpelly as 3am fazendo cimento",
+    public static Upgrade upg_cimento3 = new Upgrade("Cimento do Scarpelly3", "Krlo, Scarpelly as 3am fazendo cimento",
                     420.69f,
-                    (new object[,] {{"carlos",13},}),
-                    (new object[,] {{"+Cimento do Scarpelly",1},})
+                    (new object[,] { { "carlos", 13 }, }),
+                    (new object[,] { { "+Cimento do Scarpelly3", 1 }, })
                     );
-    public static Upgrade upg_cimento4 = new Upgrade("Cimento do Scarpelly","Krlo, Scarpelly as 3am fazendo cimento",
+    public static Upgrade upg_cimento4 = new Upgrade("Cimento do Scarpelly4", "Krlo, Scarpelly as 3am fazendo cimento",
                     420.69f,
-                    (new object[,] {{"carlos",13},}),
-                    (new object[,] {{"+Cimento do Scarpelly",1},})
+                    (new object[,] { { "carlos", 13 }, }),
+                    (new object[,] { { "+Cimento do Scarpelly4", 1 }, })
                     );
 
-    public static Upgrade[] struct_upgs = new Upgrade[] {upg_cimento,upg_cimento2,upg_cimento3,upg_cimento4};
+    public static Upgrade[] struct_upgs = new Upgrade[] { upg_cimento, upg_cimento2, upg_cimento3, upg_cimento4 };
 
 
     // Start is called before the first frame update
     void Start()
     {
 
-        
+
 
 
     }
@@ -53,7 +52,7 @@ public class ControladorGame : MonoBehaviour
     void Update()
     {
 
-        Debug.Log(upg_cimento.effects_txt_list[0,0]);
+        Debug.Log(upg_cimento.effects_txt_list[0, 0]);
 
 
     }
@@ -62,7 +61,7 @@ public class ControladorGame : MonoBehaviour
 
 public class Upgrade
 {
- 
+
     public string nome;
     public string desc;
     public float price;
@@ -70,7 +69,7 @@ public class Upgrade
     public object[,] effects_list;
     public object[,] effects_txt_list;
 
-    public Upgrade(string _nome,string _desc,float _price,  
+    public Upgrade(string _nome, string _desc, float _price,
                     object[,] _effects_list, object[,] _effects_txt_list)
     {
         nome = _nome;
--- Gulag Manager/Assets/Scripts/Upgrades/get_upgrades.cs
+++ Gulag Manager/Assets/Scripts/Upgrades/get_upgrades.cs
@@ -8,50 +8,40 @@ public class get_upgrades : MonoBehaviour
 
     public string lista;
 
-    public Upgrade[] lista_upgs = ControladorGame.struct_upgs;
+
 
     public GameObject atual;
 
-    public  upgrade_info script1;
-    public  upgrade_info script2;
-    public  upgrade_info script3;
-    public  upgrade_info script4;
+    public upgrade_info script1;
+    public upgrade_info script2;
+    public upgrade_info script3;
+    public upgrade_info script4;
 
-
+    public Upgrade[] lista_upgs;
     public upgrade_info[] lista_esp;
 
-    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject Controle = GameObject.Find("ControladorGame");
+        lista_upgs = Controle.GetComponent(lista as Upgrade[]);
 
-        GameObject upg1 = GameObject.Find("Upgrade-Info1");
-        script1 = upg1.GetComponent<upgrade_info>();
-
-        GameObject upg2 = GameObject.Find("Upgrade-Info2");
-        script2 = upg2.GetComponent<upgrade_info>();
-
-        GameObject upg3 = GameObject.Find("Upgrade-Info3");
-        script3 = upg3.GetComponent<upgrade_info>();
-
-        GameObject upg4 = GameObject.Find("Upgrade-Info4");
-        script4 = upg4.GetComponent<upgrade_info>();
-
-        upgrade_info[] lista_esp = {script1,script2,script3,script4};
+        upgrade_info[] lista_esp = { script1, script2, script3, script4 };
 
 
-        for (int i =0; i<4;i++)
+        for (int i = 0; i < 4; i++)
         {
-            lista_esp[i].nome_upg.SetText("CArlito");
+            //lista_esp[i].nome_upg.SetText(lista_upgs[0].ToString());
         }
 
 
         //for (int i=0; i<4; i++)
         {
-           // atual = lista_esp[i];
-            
+            // atual = lista_upgs[i];
+
             //atual.nome_upg.SetText(atual.nome);
             //atual.desc_upg.SetText(atual.desc);
             //atual.effect_1.SetText(atual.effects_txt_list[0,0]);
@@ -66,6 +56,5 @@ public class get_upgrades : MonoBehaviour
     void Update()
     {
 
-        
     }
 }
