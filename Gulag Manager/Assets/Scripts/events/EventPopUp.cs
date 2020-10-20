5a0a018d96f6d720c6d8ff28717f7362383832e0
Luiz Gustavo Klitzke <49824305+luizklitzke1@users.noreply.github.com>
2020-10-19 22:38:39 -0300

    Visual do pop-up dos eventos

---
+++ Gulag Manager/Assets/Scripts/events/EventPopUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPopUp : MonoBehaviour
+{
+
+    public GameObject estrutuas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        estrutuas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Gulag Manager/Assets/Scripts/events/Evento.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Evento", menuName = "Evento")]
+public class Evento: ScriptableObject
+{
+
+    public string titulo;
+    public string desc;
+
+    public string txt_op1;
+
+    public string txt_op2;
+    
+}
