7f5fa13e07934ed6442fba2a8dbe83c7e4a9d4cc
Luiz Gustavo Klitzke <49824305+luizklitzke1@users.noreply.github.com>
2020-11-30 18:28:59 -0300

    Visual ta quase kraio

---
+++ Gulag Manager/Assets/Scripts/basic_navigation2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basic_navigation2 : MonoBehaviour
+{
+
+    public GameObject obj1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown("escape"))
+        {
+            obj1.SetActive(false);
+
+        }
+    }
+}
